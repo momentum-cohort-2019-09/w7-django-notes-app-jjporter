514b2c3a05ec5b3953aec81b094a3669729e0a90
jjporter <jjporter921@gmail.com>
2019-10-22 23:14:24 -0400

    Add superuser and see notes app on admin site, but get error msg when clicking on app link

--- notey/models.py
+++ notey/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 
-# class NotesList(models.Model):
-#   title = models.CharField(max_length=100)
+class NotesList(models.Model):
+  title = models.CharField(max_length=100)
